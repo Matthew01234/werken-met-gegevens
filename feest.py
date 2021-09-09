75e388a6f8d497e3d961508606b71f8b0fc248c9
Matthew <99067338@mydavinci.nl>
2021-09-09 09:48:14 +0200

    added both files feest lunch and speelhaldag

--- feest.py
+++ feest.py
@@ -1,12 +1,10 @@
+#Feest luch kosten
+
+#Benodigheden
 crossaint = 17 * 0.39
 stokbrood= 2 * 2.78
 Kortingsbon= 3 * 0.50
 
-toegangticket= 7.45
-vipvr= 9 * 0.37
-personen= 3
-
+#berekening 
 print (crossaint + stokbrood - Kortingsbon) 
 
-#de Speelhal-dag
-print (vipvr + toegangticket * personen)
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+#de Speelhal-dag
+toegangticket= 7.45
+vipvr= 9 * 0.37
+personen= 3
+
+#berekening
+print (vipvr + toegangticket * personen)
