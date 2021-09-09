11f7607a768db1c51023b1453d49592b1f1862b7
Matthew <99067338@mydavinci.nl>
2021-09-09 10:22:34 +0200

    added pngs and modified / add files

--- feest.py
+++ feest.py
@@ -4,7 +4,10 @@
 crossaint = 17 * 0.39
 stokbrood= 2 * 2.78
 Kortingsbon= 3 * 0.50
-
+prijs= (crossaint + stokbrood) - Kortingsbon 
 #berekening 
-print (crossaint + stokbrood - Kortingsbon) 
 
+
+prijsbakker = prijs >= 10.69
+if prijsbakker:
+    print ('De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!’')
---
+++ speelhaldag2.py
@@ -0,0 +1,13 @@
+#speelhaldag2
+
+toegangticket= 7.45
+vipvr= 9 * 0.37
+personen= 4
+prijs= (vipvr + toegangticket) * personen
+#berekening
+print (prijs)
+totaalbedrag = prijs >= 43.12
+if totaalbedrag:
+    print ('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 43.12 euro')
+
+
