ca5e29d6eb904f7b5db1dfd8184e7d3239404b46
Matthew <99067338@mydavinci.nl>
2021-09-09 09:42:10 +0200

    added calculations

---
+++ feest-speelhal.py
@@ -0,0 +1,12 @@
+crossaint = 17 * 0.39
+stokbrood= 2 * 2.78
+Kortingsbon= 3 * 0.50
+
+toegangticket= 7.45
+vipvr= 9 * 0.37
+personen= 3
+
+print (crossaint + stokbrood - Kortingsbon) 
+
+#de Speelhal-dag
+print (vipvr + toegangticket * personen)
